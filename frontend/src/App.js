31ad47ed2548a55551086d81cee44bb3eba242c0
Diablo73 <ashul2173@gmail.com>
2024-02-09 15:04:36 +0530

    mf frontend ltst

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,17 @@
 import "./App.css";
 import { useState } from "react";
 import HeaderBar from "./components/HeaderBar";
-import FdComparison from "./components/FdComparison";
+import PortfolioSummary from "./components/PortfolioSummary";
 import { Avatar, TabList, Tab } from "@web3uikit/core";
 
 function App() {
 
 	const [roi, setRoi] = useState("7");
-	const [wallet, setWallet] = useState("");
-	const [chain, setChain] = useState("0x1");
-	const [totalInvestedAmount, setTotalInvestedAmount] = useState(0);
-	const [totalSimpleInterest, setTotalSimpleInterest] = useState(0);
-	const [totalCompoundInterest, setTotalCompoundInterest] = useState(0);
-	const [nativeValue, setNativeValue] = useState(0);
-	const [nativeBalance, setNativeBalance] = useState(0);
-	const [fdComparison, setFdComparison] = useState(0);
-	const [tokens, setTokens] = useState([]);
-	const [transfers, setTransfers] = useState([]);
-	const [nfts, setNfts] = useState([]);
-	const [filteredNfts, setFilteredNfts] = useState([]);
+	// const [totalInvestedAmount, setTotalInvestedAmount] = useState(0);
+	// const [totalSimpleInterest, setTotalSimpleInterest] = useState(0);
+	// const [totalCompoundInterest, setTotalCompoundInterest] = useState(0);
+	const [fdComparison, setFdComparison] = useState({});
+	const [ltstSummary, setLtstSummary] = useState([]);
 
 
 	return (
@@ -29,11 +22,13 @@ function App() {
 			/>
 			<div className="content">
 				<TabList>
-					<Tab tabKey={1} tabName={"FD Comparison"}>
-						<FdComparison
+					<Tab tabKey={1} tabName={"Portfolio Summary"}>
+						<PortfolioSummary
 							roi={roi}
 							fdComparison={fdComparison}
 							setFdComparison={setFdComparison}
+							ltstSummary={ltstSummary}
+							setLtstSummary={setLtstSummary}
 						/>
 					</Tab>
 					{/* <Tab tabKey={2} tabName={"Transfers"}>
